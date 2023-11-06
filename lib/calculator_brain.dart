c08ba6256cd86a6cf9772048ea4dc4b071a17a29
Amit Mishra <amitmishra0323@gmail.com>
2023-11-06 13:16:16 +0530

    added Screenshots

--- lib/calculator_brain.dart
+++ lib/calculator_brain.dart
@@ -17,9 +17,11 @@ class CalculatorBrain {
   String getResult() {
     if (bmi >= 25) {
       return 'Overweight';
-    } else if (bmi > 18.5) {
+    }
+    else if (bmi > 18.5) {
       return 'Normal';
-    } else {
+    }
+    else {
       return 'Underweight';
     }
   }
@@ -27,9 +29,11 @@ class CalculatorBrain {
   String getInterpretation() {
     if (bmi >= 25) {
       return "You have a higher than normal body weight. Try to exercise more.";
-    } else if (bmi >= 18.5) {
+    }
+    else if (bmi >= 18.5) {
       return "You have a normal body weight. Good job!";
-    } else {
+    }
+    else {
       return "You have a lower than normal body weight. You can eat a bit more.";
     }
   }
